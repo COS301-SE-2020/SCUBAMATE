f3ed0dbb26a33e6ecb34be9d90d2cf40e8d9e22b
CarmenJvR <50364617+CarmenJvR@users.noreply.github.com>
2020-07-13 12:26:56 +0200

    SignUp FormControl Updated

--- src/app/signup/signup.page.ts
+++ src/app/signup/signup.page.ts
@@ -9,7 +9,12 @@ import { Router } from '@angular/router';
 
 
 
-export interface SignUpClass {
+//forms
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+import { AlertController } from '@ionic/angular';
+
+export interface SignUpDiver {
   AccountGuid: string;
   DateOfBirth : string ;
   Email: string ;
@@ -22,7 +27,7 @@ export interface SignUpClass {
   Qualification: string;
 }
 
-export interface SignUpClassI {
+export interface SignInstructor {
   AccountGuid: string;
   DateOfBirth : string ;
   Email: string ;
@@ -38,6 +43,8 @@ export interface SignUpClassI {
 }
 
 
+
+
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.page.html',
@@ -45,33 +52,113 @@ export interface SignUpClassI {
 })
 export class SignupPage implements OnInit {
 
-  /*********************************************
+
+ /*********************************************
                 Global Variables
   *********************************************/
-  uuidValue:string;
-  base64textString : string;
-  showLoading : Boolean = false; 
-  showSpecialization: Boolean = false;
-  SpecializationLst : string[];
-  QualificationLst: string[];
-  CenterLst : string[];
-  userSpecialisation : string[];
+ uuidValue:string;
+ base64textString : string;
+ showLoading : Boolean = false; 
+ showSpecialization: Boolean = false;
+ SpecializationLst : string[];
+ QualificationLst: string[];
+ CenterLst : string[];
+ userSpecialisation : string[];
+ specialisationInputField: string = "";
 
   //which type of account
   signUpDiver: Boolean = false;
   signUpInstructor: Boolean = false ; 
   ShowAccountChoice : Boolean = true;
 
+   //Form Groups
+  diverForm;
+  instructorForm;
+
+  //Signup Objects 
+  diverObj: SignUpDiver;
+  instructorObj  : SignInstructor; 
+
+  /********************************************/
+
+  matchingPasswords(passwordKey: string, passwordConfirmationKey: string ) {
+    return (group: FormGroup) => {
+      let passwordInput = group.controls[passwordKey];
+      let passwordConfirmationInput = group.controls[passwordConfirmationKey];
+      if (passwordInput.value !== passwordConfirmationInput.value) {
+        return passwordConfirmationInput.setErrors({notEquivalent: true})
+      }
+    }
+  }
+
+  constructor(private _diveService: diveService,  private _accountService : accountService,  private router: Router, public formBuilder: FormBuilder, public alertController : AlertController) {
+
+    
+    //generate GUID
+    this.uuidValue=UUID.UUID();
 
 
-  /////////////////////////////////////////////////
+    //Diver Form
+    this.diverObj ={
+      AccountGuid: this.uuidValue, 
+      DateOfBirth : "",
+      Email: "",
+      FirstName: "",
+      LastName: "",
+      Password: "",
+      ProfilePhoto: "",
+      PublicStatus: false ,
+      Specialisation: [],
+      Qualification: "" 
+    }
+
+    this.diverForm = formBuilder.group({
+      firstName: ['', Validators.compose([Validators.minLength(2), Validators.pattern('[a-zA-Z ]*'), Validators.required])],
+      lastName: ['', Validators.compose([Validators.minLength(2), Validators.pattern('[a-zA-Z ]*'), Validators.required])],
+      email: ['', Validators.compose([Validators.email , Validators.pattern('[A-Za-z0-9._%+-]{2,}@[a-zA-Z-_.]{2,}[.]{1}[a-zA-Z]{2,}'), Validators.required])],
+      password: ['', Validators.compose([Validators.required, Validators.minLength(6), Validators.maxLength(12), Validators.pattern('^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{6,12}$')])],
+      confirmPassword: ['', Validators.required],
+      birthday: ['', Validators.required],
+      profile: [],
+      publicStatus: [] ,
+      qualification: ['', Validators.required],
+      specialisation: []
+    }, {validator: this.matchingPasswords('password', 'confirmPassword')}); 
 
 
-  constructor(private _diveService: diveService, 
-              private _accountService : accountService, 
-              private router: Router) { }
+    //Instructor Form
+    this.instructorObj ={
+      AccountGuid: this.uuidValue, 
+      DateOfBirth : "",
+      Email: "",
+      FirstName: "",
+      LastName: "",
+      Password: "",
+      ProfilePhoto: "",
+      PublicStatus: false ,
+      InstructorNumber: "",
+      DiveCentre: "",
+      Specialisation: [],
+      Qualification: "" 
+    }
+
+    this.instructorForm = formBuilder.group({
+      firstName: ['', Validators.compose([Validators.minLength(2), Validators.pattern('[a-zA-Z ]*'), Validators.required])],
+      lastName: ['', Validators.compose([Validators.minLength(2), Validators.pattern('[a-zA-Z ]*'), Validators.required])],
+      email: ['', Validators.compose([Validators.email , Validators.pattern('[A-Za-z0-9._%+-]{2,}@[a-zA-Z-_.]{2,}[.]{1}[a-zA-Z]{2,}'), Validators.required])],
+      password: ['', Validators.compose([Validators.required, Validators.minLength(6), Validators.maxLength(12), Validators.pattern('^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{6,12}$')])],
+      confirmPassword: ['', Validators.required],
+      birthday: ['', Validators.required],
+      profile: [],
+      publicStatus: [] ,
+      qualification: ['', Validators.required],
+      specialisation: [],
+      instructorNumber: ['', Validators.compose([Validators.minLength(3), Validators.required])],
+      diveCenter: ['', Validators.required]
+    }, {validator: this.matchingPasswords('password', 'confirmPassword')}); 
 
 
+  } //End of Constructor
 
 
   ngOnInit() {
@@ -80,78 +167,6 @@ export class SignupPage implements OnInit {
     this.signUpDiver = false;
     this.signUpInstructor = false;
     this.ShowAccountChoice = true;
-  }
-
-  ShowRelatedForm(targetValue : string){
-     console.log(targetValue);
-
-     if(targetValue == "Diver"){
-       this.signUpDiver = true;
-       this.signUpInstructor = false;
-     }else{
-       this.signUpInstructor = true;
-       this.signUpDiver = false;
-     }
-  }
-
-  SpecializationListFinder(eventValue: string){
-
-    if(eventValue.length >= 2)
-   {
-       this.showLoading = true;
-       this._accountService.getSpecializations(eventValue).subscribe(
-         data => {
-           console.log(eventValue);
-             console.log(data);
-             this.SpecializationLst = data.ReturnedList ; 
-             this.showLoading = false;
-         }
-       ); //end Buddy req
-   }
-
- }
-
- QualificationListFinder(eventValue: string){
-
-  if(eventValue.length >= 2)
- {
-     this.showLoading = true;
-     this._accountService.getQualifications(eventValue).subscribe(
-       data => {
-         console.log(eventValue);
-           console.log(data);
-           this.QualificationLst = data.ReturnedList ; 
-           this.showLoading = false;
-       }
-     ); //end Buddy req
- }
-
-}
-
-  CenterListFinder(eventValue: string){
-
-    if(eventValue.length >= 2)
-   {
-       this.showLoading = true;
-       this._diveService.getDiveCenters(eventValue).subscribe(
-         data => {
-           console.log(eventValue);
-             console.log(data);
-             this.CenterLst = data.ReturnedList ; 
-             this.showLoading = false;
-         }
-       ); //end Buddy req
-   }
-  
-  }
-
-  addSpecialisation(s : string){
-    if(s != "")
-    {
-      this.userSpecialisation.push(s);
-      this.showSpecialization = true;
-    }
-    
   }
 
   onFileSelected(event) {
@@ -163,99 +178,195 @@ export class SignupPage implements OnInit {
       //console.log(reader.result);
       let s = reader.result ; 
       me.base64textString = reader.result.toString() ;
+
+      if(me.signUpDiver)
+      {
+        me.diverObj.ProfilePhoto = me.base64textString;
+      }else{
+        me.instructorObj.ProfilePhoto = me.base64textString;
+      }
+      
     };
     reader.onerror = function (error) {
       console.log('Error: ', error);
     };
  }
 
+  ShowRelatedForm(targetValue : string){
+  console.log(targetValue);
 
-  onSubmitDiver(Qual: string, Spec: string , bDay:string,  FName: string , LName: string, pub: boolean, emailI: string, Pass: string, cPass: string, event : Event) {
+  if(targetValue == "Diver"){
+    this.signUpDiver = true;
+    this.signUpInstructor = false;
+  }else{
+    this.signUpInstructor = true;
+    this.signUpDiver = false;
+  }
+}
 
-    if( Pass != cPass ) //test that passwords match
-    {
-      alert("Passwords do not match");
-    }else if(  (FName =="") ||  (LName ==="") || (emailI=="") || (Pass=="") || (cPass=="") ){  //test empty fields
-      alert("Empty fields provided. \nPlease fill in all the fields");
-    }else{
-      //generate GUID
-      this.uuidValue=UUID.UUID();
-      
-      //create object to send
-      var attemptLogin = {
-        AccountGuid : this.uuidValue,  
-        FirstName: FName,
-        LastName: LName,
-        Email: emailI,
-        DateOfBirth : bDay ,
-        Password: Pass,  
-        ProfilePhoto: this.base64textString,   //"meep.jpg",
-        PublicStatus: pub ,
-        Specialisation: this.userSpecialisation , 
-        Qualification: Qual 
-      } as SignUpClass; 
-        //send to API service 
-        console.log(attemptLogin);
+SpecializationListFinder(){
 
-        this._accountService.insertUserDiver( attemptLogin ).subscribe( res =>{
-          console.log("in res");
-          console.log(res);
-          this._accountService.sendValidationEmail(attemptLogin.Email).subscribe( res => {
-            console.log(res);
-            this.router.navigate(['login']);
-          });
-        }); 
+ if(this.specialisationInputField.length >= 2)
+{
+    this.showLoading = true;
+    this._accountService.getSpecializations(this.specialisationInputField).subscribe(
+      data => {
+        console.log("Specialisation search for: " + this.specialisationInputField);
+          console.log(data);
+          this.SpecializationLst = data.ReturnedList ; 
+          this.showLoading = false;
+      }
+    ); //end Buddy req
+}
+
+}
+
+QualificationListFinder(eventValue: string){
+
+if(eventValue.length >= 2)
+{
+  this.showLoading = true;
+  this._accountService.getQualifications(eventValue).subscribe(
+    data => {
+      console.log(eventValue);
+        console.log(data);
+        this.QualificationLst = data.ReturnedList ; 
+        this.showLoading = false;
     }
+  ); //end Buddy req
+}
 
-     
+}
+
+CenterListFinder(eventValue: string){
+
+ if(eventValue.length >= 2)
+{
+    this.showLoading = true;
+    this._diveService.getDiveCenters(eventValue).subscribe(
+      data => {
+        console.log(eventValue);
+          console.log(data);
+          this.CenterLst = data.ReturnedList ; 
+          this.showLoading = false;
+      }
+    ); //end Buddy req
+}
+
+}
+
+addSpecialisation(s : string){
+ if(s != "")
+ {
+  const index: number = this.userSpecialisation.indexOf(s);
+  if (index == -1) {
+    this.userSpecialisation.push(s);
+    this.showSpecialization = true;
+  }
+   this.specialisationInputField = "";
+ }
+
+ console.log("Specialisation Added: ");
+ console.log(this.userSpecialisation);
+ 
+}
+
+removeSpecialisation(s : string){
+  const index: number = this.userSpecialisation.indexOf(s);
+  if (index !== -1) {
+    this.userSpecialisation.splice(index, 1);
+  }  
+
+  this.SpecializationLst = [] ;
+}
+
+async presentAlert() {
+  const alert = await this.alertController.create({
+    cssClass: 'errorAlert',
+    header: 'Invalid Signup',
+    message: 'Please provide all required information to complete the signup',
+    buttons: ['OK']
+  });
+
+  await alert.present();
+}
+
+async presentAlertEmail() {
+  const alert = await this.alertController.create({
+    cssClass: 'errorAlert',
+    header: 'Invalid Signup',
+    message: 'Email is already in use. <br> Please provide a different Email',
+    buttons: ['OK']
+  });
+
+  await alert.present();
+}
+
+async presentAlertEmailSent( userEmail ) {
+  const alert = await this.alertController.create({
+    cssClass: 'errorAlert',
+    header: 'Signup Successful',
+    subHeader: 'Please validate Email',
+    message: 'An OTP has been sent to: <br>' + userEmail ,
+    buttons: ['OK']
+  });
+
+  await alert.present();
+}
+
+
+DiverSubmit(){
+  console.log(this.diverObj);
+
+  if(!this.diverForm.valid || this.userSpecialisation.length == 0 ){
+    this.presentAlert();
+  }else{
+    this.diverObj.Specialisation = this.userSpecialisation ;
+    
+    this._accountService.insertUserDiver( this.diverObj ).subscribe( res =>{
+      console.log("Sending Diver Info");
+      
+      this.showLoading = true;
+      this._accountService.sendValidationEmail(this.diverObj.Email).subscribe( res => {
+        this.showLoading = false;
+        this.presentAlertEmailSent(this.diverObj.Email);
+
+        this.router.navigate(['login']);
+      });
+    },  err => this.presentAlertEmail()); 
+
+
   }
 
 
-  onSubmitInstructor(qual: string, cent: string, INum: string , bDay:string,  FName: string , LName: string, pub: boolean, emailI: string, Pass: string, cPass: string, event : Event) {
+}
 
-    if( Pass != cPass ) //test that passwords match
-    {
-      alert("Passwords do not match");
-    }else if(  (FName =="") ||  (LName ==="") || (emailI=="") || (Pass=="") || (cPass=="") ){  //test empty fields
-      alert("Empty fields provided. \nPlease fill in all the fields");
-    }else{
-      //generate GUID
-      this.uuidValue=UUID.UUID();
+
+InstructorSubmit(){
+
+  if(!this.instructorForm.valid || this.userSpecialisation.length == 0 ){
+    this.presentAlert();
+  }else{
+    this.instructorObj.Specialisation = this.userSpecialisation ;
+    console.log(this.instructorObj);
+    
+    this._accountService.insertUserInstructor( this.instructorObj ).subscribe( res =>{
+      console.log("Sending Instructor Info");
       
-      //create object to send
-      var attemptSignUp = {
-        AccountGuid : this.uuidValue,  
-        FirstName: FName,
-        LastName: LName,
-        Email: emailI,
-        DateOfBirth : bDay ,
-        Password: Pass,  
-        ProfilePhoto: this.base64textString,   //"meep.jpg",
-        PublicStatus: pub ,
-        InstructorNumber: INum,
-        DiveCentre: cent,
-        Specialisation: this.userSpecialisation,
-        Qualification: qual 
-      } as SignUpClassI; 
-        //send to API service 
-        console.log(attemptSignUp);
+      this.showLoading = true;
+      this._accountService.sendValidationEmail(this.instructorObj.Email).subscribe( res => {
+        this.showLoading = false;
+        this.presentAlertEmailSent(this.instructorObj.Email);
 
-        this._accountService.insertUserInstructor( attemptSignUp ).subscribe( res =>{
-          console.log("in res");
-          console.log(res);
-          this._accountService.sendValidationEmail(attemptSignUp.Email).subscribe( res => {
-            console.log(res);
-            this.router.navigate(['login']);
-          });
-         
-        }); 
-    }
+        this.router.navigate(['login']);
+      });
+    },  err => this.presentAlertEmail()); 
 
-     
+
   }
+}
 
-
-
+  
 
 
 }
