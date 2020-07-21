c45e98ecd5e7abfb271e96bc33e18eb88731b569
GiseleNadine <u18012583@tuks.co.za>
2020-07-21 14:06:43 +0200

    Updated for code standards

--- AWScode/getPublicDiveLogs/index.js
+++ AWScode/getPublicDiveLogs/index.js
@@ -3,114 +3,78 @@ const AWS = require('aws-sdk');
 AWS.config.update({region: "af-south-1"});
 
 exports.handler = async (event, context) => {
-    const ddb = new AWS.DynamoDB({apiVersion:"2012-08-10"});
-    const documentClient = new AWS.DynamoDB.DocumentClient({region: "af-south-1"});
 
-    let responseBody = "";
-    let statusCode =0;
-    // let publicLogs = "";
-
-    let body = JSON.parse(event.body)
+    const body = JSON.parse(event.body);
     const AccessToken = body.AccessToken; 
     console.log(body.AccessToken);
-    const guid = AccessToken.substring(0,36);
+    const GuidSize = 36;
+    const AccountGuid = AccessToken.substring(0,GuidSize);
 
-    function compareDates(t,e)
-    {
-        console.log(t.getFullYear());
-        if(t.getFullYear()<e.getFullYear())
-        {   
-            return false;
-        }else if(t.getFullYear()>e.getFullYear())
-        {
-            return true;
-        }
-
-        if(t.getMonth()<e.getMonth())
-        {
-            return false;
-        }else if(t.getMonth()>e.getMonth())
-        {
-            return true;
-        }
-
-        if(t.getDate()<e.getDate())
-        {
-            return false;
-        }else if(t.getDate()>e.getDate())
-        {
-            return true;
-        }
-
-        if(t.getHours()<e.getHours())
-        {
-            return false;
-        }else if(t.getHours()>e.getHours())
-        {
-            return true;
-        }
-
-        if(t.getMinutes()<e.getMinutes())
-        {
-            return false;
-        }else if(t.getMinutes()>e.getMinutes())
-        {
-            return true;
-        }
-
-        if(t.getSeconds()<e.getSeconds())
-        {
-            return false;
-        }else if(t.getSeconds()>e.getSeconds())
-        {
-            return true;
-        }
-
-        if(t.getMilliseconds()<e.getMilliseconds())
-        {
-            return false;
-        }else if(t.getMilliseconds()>e.getMilliseconds())
-        {
-            return true;
-        }
-
-        return true;
-    }
-
-    //Verify AccessToken 
+    /* Verify AccessToken  */
     const params = {
         TableName: "Scubamate",
         Key: {
-            "AccountGuid": guid
+            "AccountGuid": AccountGuid
         }
+    };
+    
+    function compareDates(t,e){
+        let returnBool;
+        if(t.getFullYear()!=e.getFullYear()){
+            (t.getFullYear()>e.getFullYear())?returnBool=true:returnBool=false;
+        }   
+        else if(t.getMonth()!=e.getMonth()){
+            (t.getMonth()>e.getMonth())?returnBool=true:returnBool=false;
+        }
+        else if(t.getDate()!=e.getDate()){
+            (t.getDate()>e.getDate())?returnBool=true:returnBool=false;
+        }
+        else if(t.getHours()!=e.getHours()){
+            (t.getHours()>e.getHours())?returnBool=true:returnBool=false;
+        }
+        else if(t.getMinutes()!=e.getMinutes()){
+            (t.getMinutes()>e.getMinutes())?returnBool=true:returnBool=false;
+        }
+        else if(t.getSeconds()!=e.getSeconds()){
+            (t.getSeconds()>e.getSeconds())?returnBool=true:returnBool=false;
+        }
+        else if(t.getMilliseconds()!=e.getMilliseconds()){
+            (t.getMilliseconds()>e.getMilliseconds())?returnBool=true:returnBool=false;
+        }
+        else{
+            returnBool = true;
+        }
+        return returnBool;
     }
-
+    let responseBody;
+    const undef = 0;
+    let statusCode = undef;
+    const documentClient = new AWS.DynamoDB.DocumentClient({region: "af-south-1"});
+    
     try {     
         const data = await documentClient.get(params).promise();
         
-        if( data.Item.Expires) // check if it's undefined
-        {
+        if((data.Item.AccessToken).toString().trim() != AccessToken){
+            statusCode = 403;
+            responseBody = "Invalid Access Token" ;
+        }
+        else if(data.Item.Expires){
             const expiryDate = new Date(data.Item.Expires);
             const today = new Date();
-            console.log("Compare: " + today + " and " + expiryDate  + " " + compareDates(today,expiryDate));
             if(compareDates(today,expiryDate))
             {
-                statusCode = 403;
                 responseBody = "Access Token Expired!";
-            }
-                
+                statusCode = 403;
+            }  
         }
-            
-        console.log("status is now: " + statusCode) ;
 
-    } catch (error) {
-        console.error(error);
+    } catch (err) {
         statusCode = 403;
-        responseBody = "Invalid Access Token ";
+        responseBody = "Invalid Access Token";
     }
 
     //Only proceed if access token is valid
-    if(statusCode==0){
+    if(statusCode==undef){
 
         var diveParams = {
             TableName: "Dives",
@@ -228,8 +192,6 @@ exports.handler = async (event, context) => {
         }
         
     }   
-    
-  
 
     const response = {
         statusCode: statusCode,
@@ -246,4 +208,3 @@ exports.handler = async (event, context) => {
     return response;
 
 };
-
