7d3f9f2dbe335333dd326c516eeda84f97b51f33
GiseleNadine <u18012583@tuks.co.za>
2020-07-21 20:12:06 +0200

    Added syntax

--- AWScode/editAccount/index.js
+++ AWScode/editAccount/index.js
@@ -1,139 +1,133 @@
 'use strict'
 const AWS = require('aws-sdk');
 AWS.config.update({region: "af-south-1"});
-const documentClient = new AWS.DynamoDB.DocumentClient({region: "af-south-1"});
-//const s3 = new AWS.S3({apiVersion: '2006-03-01'});
 
 exports.handler = async (event, context, callback) => {
 
-    let responseBody = "";
-    let statusCode =0;
-
-    let body = JSON.parse(event.body);
+    const body = JSON.parse(event.body);
     const AccessToken = body.AccessToken;
     const FirstName = body.FirstName;
     const LastName = body.LastName;
     const DateOfBirth = body.DateOfBirth;
     const ProfilePhoto = body.ProfilePhoto;
     const PublicStatus = body.PublicStatus;
-    
 
-     const guid = AccessToken.substring(0,36);
+    const GuidSize = 36;
+    const AccountGuid = AccessToken.substring(0,GuidSize);
 
-    function compareDates(t,e)
-    {
-        console.log(t.getFullYear());
-        if(t.getFullYear()<e.getFullYear())
-        {   
-            return false;
-        }else if(t.getFullYear()>e.getFullYear())
-        {
-            return true;
-        }
-
-        if(t.getMonth()<e.getMonth())
-        {
-            return false;
-        }else if(t.getMonth()>e.getMonth())
-        {
-            return true;
-        }
-
-        if(t.getDate()<e.getDate())
-        {
-            return false;
-        }else if(t.getDate()>e.getDate())
-        {
-            return true;
-        }
-
-        if(t.getHours()<e.getHours())
-        {
-            return false;
-        }else if(t.getHours()>e.getHours())
-        {
-            return true;
-        }
-
-        if(t.getMinutes()<e.getMinutes())
-        {
-            return false;
-        }else if(t.getMinutes()>e.getMinutes())
-        {
-            return true;
-        }
-
-        if(t.getSeconds()<e.getSeconds())
-        {
-            return false;
-        }else if(t.getSeconds()>e.getSeconds())
-        {
-            return true;
-        }
-
-        if(t.getMilliseconds()<e.getMilliseconds())
-        {
-            return false;
-        }else if(t.getMilliseconds()>e.getMilliseconds())
-        {
-            return true;
-        }
-
-        return true;
-    }
-
-    //Verify AccessToken 
+    /* Verify AccessToken  */
     const params = {
         TableName: "Scubamate",
         Key: {
-            "AccountGuid": guid
+            "AccountGuid": AccountGuid
         }
+    };
+    
+    function compareDates(t,e){
+        let returnBool;
+        if(t.getFullYear()!=e.getFullYear()){
+            (t.getFullYear()>e.getFullYear())?returnBool=true:returnBool=false;
+        }   
+        else if(t.getMonth()!=e.getMonth()){
+            (t.getMonth()>e.getMonth())?returnBool=true:returnBool=false;
+        }
+        else if(t.getDate()!=e.getDate()){
+            (t.getDate()>e.getDate())?returnBool=true:returnBool=false;
+        }
+        else if(t.getHours()!=e.getHours()){
+            (t.getHours()>e.getHours())?returnBool=true:returnBool=false;
+        }
+        else if(t.getMinutes()!=e.getMinutes()){
+            (t.getMinutes()>e.getMinutes())?returnBool=true:returnBool=false;
+        }
+        else if(t.getSeconds()!=e.getSeconds()){
+            (t.getSeconds()>e.getSeconds())?returnBool=true:returnBool=false;
+        }
+        else if(t.getMilliseconds()!=e.getMilliseconds()){
+            (t.getMilliseconds()>e.getMilliseconds())?returnBool=true:returnBool=false;
+        }
+        else{
+            returnBool = true;
+        }
+        return returnBool;
     }
-
+    let responseBody;
+    const undef = 0;
+    let statusCode = undef;
+    const documentClient = new AWS.DynamoDB.DocumentClient({region: "af-south-1"});
+    
     try {     
         const data = await documentClient.get(params).promise();
         
-        if( data.Item.Expires) // check if it's undefined
-        {
+        if((data.Item.AccessToken).toString().trim() != AccessToken){
+            statusCode = 403;
+            responseBody = "Invalid Access Token" ;
+        }
+        else if(data.Item.Expires){
             const expiryDate = new Date(data.Item.Expires);
             const today = new Date();
-            console.log("Compare: " + today + " and " + expiryDate  + " " + compareDates(today,expiryDate));
             if(compareDates(today,expiryDate))
             {
-                statusCode = 403;
                 responseBody = "Access Token Expired!";
-            }
-                
+                statusCode = 403;
+            }  
         }
-            
-        console.log("status is now: " + statusCode) ;
 
-    } catch (error) {
-        console.error(error);
+    } catch (err) {
         statusCode = 403;
-        responseBody = "Invalid Access Token ";
+        responseBody = "Invalid Access Token";
     }
-
-    // Only update account if access token is verified
-    if(statusCode==0){
+    /* Put new image */
+    /* data:image/png;base64, is send at the front of ProfilePhoto thus find the first , */
+    const startContentType = ProfilePhoto.indexOf(":")+1;
+    const endContentType = ProfilePhoto.indexOf(";");
+    const contentType = ProfilePhoto.substring(startContentType, endContentType);
     
-        //update profile image 
-        var profileLink = ProfilePhoto; //to be added later
-        
+    const startExt = contentType.indexOf("/")+1;
+    const extension = contentType.substring(startExt, contentType.length);
+    
+    const startIndex = ProfilePhoto.indexOf(",")+1;
+    
+    const encodedImage = ProfilePhoto.substring(startIndex, ProfilePhoto.length);
+    const decodedImage = Buffer.from(encodedImage.replace(/^data:image\/\w+;base64,/, ""),'base64');
+  
+    const filePath = "profilephoto" + AccountGuid + "."+extension;
+    
+    let profileLink ="https://profilephoto-imagedatabase-scubamate.s3.af-south-1.amazonaws.com/"+filePath;
+
+    const paramsImage = {
+      "Body": decodedImage,
+      "Bucket": "profilephoto-imagedatabase-scubamate",
+      "Key": filePath,
+      "ContentEncoding": 'base64',
+      "ContentType" : contentType
+    };
+    
+    const s3 = new AWS.S3({apiVersion: '2006-03-01'});
+    s3.putObject(paramsImage, function(err, data){
+        if(err) {
+            responseBody = "Could not update image.";
+            statusCode = 500;
+        }
+    });
+    
+    
+    /* Only update account if access token is verified */
+    if(statusCode==undef){
         const params = {
             TableName: "Scubamate",
             Key: {
-                'AccountGuid' : guid,
+                'AccountGuid' : AccountGuid,
             },
-            UpdateExpression: 'set FirstName = :f, LastName = :l, DateOfBirth = :d, ProfilePhoto = :pp, PublicStatus = :ps',
+            UpdateExpression: 'set FirstName = :f, LastName = :l, DateOfBirth = :d, PublicStatus = :ps, ProfilePhoto = :pl',
             ExpressionAttributeValues: {
                 ':f' : FirstName,
                 ':l' :LastName,
                 ':d': DateOfBirth,
-                ':pp': profileLink,
-                ':ps': PublicStatus
+                ':ps': PublicStatus,
+                ':pl': profileLink
             }
-        }
+        };
         try{
             const data = await documentClient.update(params).promise();
             responseBody = "Successfully updated account!";
@@ -154,10 +148,7 @@ exports.handler = async (event, context, callback) => {
         },
         body : JSON.stringify(responseBody),
         isBase64Encoded: false
-    }
+    };
     return response;
     
-}
-
-
-
+};
