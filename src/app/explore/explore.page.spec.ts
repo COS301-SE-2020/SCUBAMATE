2bc99c949fc9e4b8127edd9c83924a4d1fc85d2d
u17021627 <u17021627@tuks.co.za>
2020-07-21 15:24:01 +0200

    Unit/Integrated Test Explore V.01

--- src/app/explore/explore.page.spec.ts
+++ src/app/explore/explore.page.spec.ts
@@ -4,7 +4,7 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { ExplorePage } from './explore.page';
 import { AppModule } from '../app.module';
 
-describe('ExplorePage', () => {
+fdescribe('ExplorePage', () => {
   let component: ExplorePage;
   let fixture: ComponentFixture<ExplorePage>;
   let responseCode = 200;
@@ -22,5 +22,25 @@ describe('ExplorePage', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  fit('Testing Explore Components', () => {
+    //expect(component.siteLst).toBeDefined();
+    //expect(component.centerLst).toBeDefined();
+    expect(component.showSites).toBeFalse();
+    expect(component.showCenters).toBeFalse();
+    expect(component.showFeed).toBeTrue();
+    expect(component.showLoading).toBeTrue();
+    //expect(component.pubLst).toBeDefined();
+    expect(component.loginLabel).toBeDefined();
+  });
+
+  fit('Testing Explore Functionlity', () => {
+    //expect(component.ngOnInit).toBeDefined();
+    //expect(component.ionViewWillEnter).toBeDefined();
+    expect(component.loginClick).toBeDefined();
+    expect(component.displayDiveSites).toBeDefined();
+    expect(component.displayDiveCenters).toBeDefined();
+    expect(component.displayFeed).toBeDefined();
   });
 });
