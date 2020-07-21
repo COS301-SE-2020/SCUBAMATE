aafeff0ba8125e346bc2a7e44885f3bb2e349655
u17021627 <u17021627@tuks.co.za>
2020-07-21 16:19:29 +0200

    Unit/Integrated Test SignUp V.01

--- src/app/signup/signup.page.spec.ts
+++ src/app/signup/signup.page.spec.ts
@@ -4,7 +4,7 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { SignupPage } from './signup.page';
 import { AppModule } from '../app.module';
 
-describe('SignupPage', () => {
+fdescribe('SignupPage', () => {
   let component: SignupPage;
   let fixture: ComponentFixture<SignupPage>;
 
@@ -19,7 +19,33 @@ describe('SignupPage', () => {
     fixture.detectChanges();
   }));
 
-  it('should create', () => {
+  fit('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  fit('Testing SignUp Components', () => {
+    //expect(component.uuidValue).toBeDefined();
+    //expect(component.base64textString).toBeDefined();
+    expect(component.showLoading).toBeFalse();
+    expect(component.showSpecialization).toBeFalse();
+    //expect(component.SpecializationLst).toBeDefined();
+    //expect(component.QualificationLst).toBeDefined();
+    //expect(component.CenterLst).toBeDefined();
+    expect(component.userSpecialisation).toBeDefined();
+    expect(component.signUpDiver).toBeFalse();
+    expect(component.signUpInstructor).toBeFalse();
+    //expect(component.ShowAccountChoice).toBeFalse();
+  });
+
+  fit('Testing SignUp Functionality', () => {
+    expect(component.ngOnInit).toBeTruthy();
+    expect(component.ShowRelatedForm).toBeTruthy();
+    expect(component.SpecializationListFinder).toBeTruthy();
+    expect(component.QualificationListFinder).toBeTruthy();
+    expect(component.CenterListFinder).toBeTruthy();
+    expect(component.addSpecialisation).toBeTruthy();
+    expect(component.onFileSelected).toBeTruthy();
+    expect(component.onSubmitDiver).toBeTruthy();
+    expect(component.onSubmitInstructor).toBeTruthy();
   });
 });
