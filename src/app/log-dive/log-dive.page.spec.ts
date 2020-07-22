6e112d17097aacc85870b6092e2d5880ca823265
u17021627 <u17021627@tuks.co.za>
2020-07-22 17:07:55 +0200

    Unit/Integrated Test Log-Dive V.02

--- src/app/log-dive/log-dive.page.spec.ts
+++ src/app/log-dive/log-dive.page.spec.ts
@@ -19,26 +19,65 @@ describe('LogDivePage', () => {
     fixture.detectChanges();
   }));
 
-  it('should create', () => {
+  it('Succesfully Created Log-Dive Page', () => {
     expect(component).toBeTruthy();
   });
 
   it('Testing Log-Dive Components', () => {
-    //expect(component.uuidValue).toBeInstanceOf(String);
+    expect(component.uuidValue).toBeUndefined();
     expect(component.showLoading).toBeFalse();
     expect(component.DiveTypeLst).toBeDefined();
     expect(component.DiveSiteLst).toBeDefined();
-    //expect(component.BuddyLst).toBeDefined();
+    expect(component.BuddyLst).toBeUndefined();
     expect(component.cDate).toBeDefined();
     expect(component.currentDate).toBeDefined();
-    //expect(component.MaxTempAPI).toBeDefined();
-    //expect(component.MinTempAPI).toBeDefined();
-    //expect(component.MoonPhase).toBeDefined();
-    //expect(component.WeatherDescription).toBeDefined();
-    //expect(component.WindSpeed).toBeDefined();
+    expect(component.MaxTempAPI).toBeUndefined();
+    expect(component.MinTempAPI).toBeUndefined();
+    expect(component.MoonPhase).toBeUndefined();
+    expect(component.WeatherDescription).toBeUndefined();
+    expect(component.WindSpeed).toBeUndefined();
     expect(component.Key).toBeDefined();
     expect(component.Coordinates).toBeDefined();
     expect(component.loginLabel).toBeDefined();
+  });
+
+  it('Testing ngOnInit()', () => {
+    component.ngOnInit();
+    expect(component.cDate).toBeInstanceOf(Date);
+    expect(component.currentDate).toBeDefined();
+    expect(component.showLoading).toBeTrue();
+    expect(component.loginLabel).toBe("Login");
+    expect(component.DiveSiteLst).toBeDefined();
+    expect(component.DiveTypeLst).toBeDefined();
+    expect(component.Coordinates).toBeDefined();
+    expect(component.Key).toBeDefined();
+    expect(component.MaxTempAPI).toBeUndefined();
+    expect(component.MinTempAPI).toBeUndefined();
+    expect(component.MoonPhase).toBeUndefined();
+    expect(component.WeatherDescription).toBeUndefined();
+    expect(component.WindSpeed).toBeUndefined();
+    
+  });
+
+  it('Testing ionViewWillEnter()', () => {
+    component.ionViewWillEnter();
+    expect(component.loginLabel).toBe("Login");
+  });
+
+  it('Testing loginClick()', () => {
+    component.loginClick();
+  });
+
+  it('Testing onSubmit()', () => {
+    component.onSubmit(false, "", "", "", "", "", "", "", "", "", 0, 0, 0, event);
+    expect(component.uuidValue).toBeDefined();
+    expect(component.showLoading).toBeFalse();
+  });
+
+  it('Testing buddyListFinder()', () => {
+    component.buddyListFinder("");
+    expect(component.BuddyLst).toBeUndefined();
+    expect(component.showLoading).toBeFalse();
   });
 
   it('Testing Log-Dive Functionality', () => {
