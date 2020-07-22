7090a91951205b366037bab181e7311e65cf9e2d
u17021627 <u17021627@tuks.co.za>
2020-07-22 20:58:50 +0200

    Unit/Integrated Test Home V.03

--- src/app/home/home.page.spec.ts
+++ src/app/home/home.page.spec.ts
@@ -3,50 +3,60 @@ import { IonicModule } from '@ionic/angular';
 import { RouterTestingModule } from '@angular/router/testing';
 import { HomePage } from './home.page';
 import { AppModule } from '../app.module';
+import { accountService } from '../service/account.service';
+import { HttpClient} from '@angular/common/http';
+import { Router } from '@angular/router';
 
-describe('HomePage', () => {
+fdescribe('HomePage', () => {
   let component: HomePage;
   let fixture: ComponentFixture<HomePage>;
+  let accService: accountService;
+  let http: HttpClient;
+  let router; Router;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ HomePage ],
-      imports: [IonicModule.forRoot(), RouterTestingModule, AppModule]
+      imports: [IonicModule.forRoot(), RouterTestingModule, AppModule],
+      providers: [accountService]
     }).compileComponents();
 
     fixture = TestBed.createComponent(HomePage);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    accService = new accountService(http, router);
   }));
 
-  it('Successfully Created Home Page', () => {
+  fit('Successfully Created Home Page', () => {
     expect(component).toBeTruthy();
   });
 
-  it('Testing Home Components', () => {
+  fit('Testing Home Components', () => {
     expect(component.siteLst).toBeUndefined();
     expect(component.loginLabel).toBeDefined();
   });
 
-  it('Testing ngOnInit()', () => {
+  fit('Testing ngOnInit()', () => {
     component.ngOnInit();
     expect(component.loginLabel).toBe("Login");
   });
 
-  it('Testing ionViewWillEnter()', () => {
+  fit('Testing ionViewWillEnter()', () => {
     component.ionViewWillEnter();
     expect(component.loginLabel).toBe("Login");
   });
 
-  it('Testing loginClick()', () => {
+  fit('Testing loginClick()', () => {
     component.loginClick();
   });
 
-  it('Testing sendEmail()', () => {
+  fit('Testing sendEmail()', () => {
     component.sendEmail();
+    let accountSpy = spyOn(accService, 'sendValidationEmail').and.callThrough();
+    expect(accountSpy).toBeDefined();
   });
 
-  it('Testing Home Functionality', () => {
+  fit('Testing Home Functionality', () => {
     expect(component.ngOnInit).toBeTruthy();
     expect(component.ionViewWillEnter).toBeTruthy();
     expect(component.loginClick).toBeTruthy();
