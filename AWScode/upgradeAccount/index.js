d8dbe812ca854c7c6f104ddf4dc07469210a9118
GiseleNadine <u18012583@tuks.co.za>
2020-07-30 19:02:45 +0200

    Added verification checks

--- AWScode/upgradeAccount/index.js
+++ AWScode/upgradeAccount/index.js
@@ -1,133 +1,107 @@
 'use strict'
 const AWS = require('aws-sdk');
 AWS.config.update({region: "af-south-1"});
-const documentClient = new AWS.DynamoDB.DocumentClient({region: "af-south-1"});
 
 exports.handler = async (event, context, callback) => {
 
-    let responseBody = "";
-    let statusCode =0;
-
-//Adding new instructor
-    let body = JSON.parse(event.body);
+    const body = JSON.parse(event.body);
     
     const InstructorNumber = body.InstructorNumber;
     const DiveCentre = body.DiveCentre;
     const AccessToken = body.AccessToken;
+    const GuidSize = 36;
+    const AccountGuid = AccessToken.substring(0,GuidSize);
     
-    function compareDates(t,e)
-    {
-        console.log(t.getFullYear());
-        if(t.getFullYear()<e.getFullYear())
-        {   
-            return false;
-        }else if(t.getFullYear()>e.getFullYear())
-        {
-            return true;
-        }
-
-        if(t.getMonth()<e.getMonth())
-        {
-            return false;
-        }else if(t.getMonth()>e.getMonth())
-        {
-            return true;
-        }
-
-        if(t.getDate()<e.getDate())
-        {
-            return false;
-        }else if(t.getDate()>e.getDate())
-        {
-            return true;
-        }
-
-        if(t.getHours()<e.getHours())
-        {
-            return false;
-        }else if(t.getHours()>e.getHours())
-        {
-            return true;
-        }
-
-        if(t.getMinutes()<e.getMinutes())
-        {
-            return false;
-        }else if(t.getMinutes()>e.getMinutes())
-        {
-            return true;
-        }
-
-        if(t.getSeconds()<e.getSeconds())
-        {
-            return false;
-        }else if(t.getSeconds()>e.getSeconds())
-        {
-            return true;
-        }
-
-        if(t.getMilliseconds()<e.getMilliseconds())
-        {
-            return false;
-        }else if(t.getMilliseconds()>e.getMilliseconds())
-        {
-            return true;
-        }
-
-        return true;
-    }
-
-    //Verify AccessToken 
-    const guid = AccessToken.substring(0,36);
+    /* Verify AdminAccessToken  */
     const params = {
         TableName: "Scubamate",
         Key: {
-            "AccountGuid": guid
+            "AccountGuid": AccountGuid
         }
+    };
+    
+    function compareDates(t,e){
+        let returnBool;
+        if(t.getFullYear()!=e.getFullYear()){
+            (t.getFullYear()>e.getFullYear())?returnBool=true:returnBool=false;
+        }   
+        else if(t.getMonth()!=e.getMonth()){
+            (t.getMonth()>e.getMonth())?returnBool=true:returnBool=false;
+        }
+        else if(t.getDate()!=e.getDate()){
+            (t.getDate()>e.getDate())?returnBool=true:returnBool=false;
+        }
+        else if(t.getHours()!=e.getHours()){
+            (t.getHours()>e.getHours())?returnBool=true:returnBool=false;
+        }
+        else if(t.getMinutes()!=e.getMinutes()){
+            (t.getMinutes()>e.getMinutes())?returnBool=true:returnBool=false;
+        }
+        else if(t.getSeconds()!=e.getSeconds()){
+            (t.getSeconds()>e.getSeconds())?returnBool=true:returnBool=false;
+        }
+        else if(t.getMilliseconds()!=e.getMilliseconds()){
+            (t.getMilliseconds()>e.getMilliseconds())?returnBool=true:returnBool=false;
+        }
+        else{
+            returnBool = true;
+        }
+        return returnBool;
     }
-
+    let responseBody;
+    const undef = 0;
+    let statusCode = undef;
+    const documentClient = new AWS.DynamoDB.DocumentClient({region: "af-south-1"});
+    
     try {     
         const data = await documentClient.get(params).promise();
         
-        if( data.Item.Expires) // check if it's undefined
-        {
+        if((data.Item.AccessToken).toString().trim() != AccessToken ){
+            statusCode = 403;
+            responseBody = "Invalid Access Token" ;
+        }
+        else if(data.Item.Expires){
             const expiryDate = new Date(data.Item.Expires);
             const today = new Date();
-            console.log("Compare: " + today + " and " + expiryDate  + " " + compareDates(today,expiryDate));
             if(compareDates(today,expiryDate))
             {
-                statusCode = 403;
                 responseBody = "Access Token Expired!";
-            }
-                
+                statusCode = 403;
+            }  
         }
-            
-        console.log("status is now: " + statusCode) ;
+        else if(data.Item.AccountVerified){
+            statusCode = 403;
+            responseBody = "Account Not Verified by Admin - Ask "+DiveCentre+" to Verify";
+        }
+        else if(data.Item.EmailVerified){
+            statusCode = 403;
+            responseBody = "Account Email Not Verified - Can't Upgrade Until Email Is Verified";
+        }
 
-    } catch (error) {
-        console.error(error);
+    } catch (err) {
         statusCode = 403;
-        responseBody = "Invalid Access Token ";
+        responseBody = "Invalid Access Token";
     }
-    
 
-    //Only proceed if access token is valid
-    if(statusCode==0){
+    /* Only upgrade account if access token is verified */
+    if(statusCode==undef){
         
         const AccountType = "Instructor";
 
         const params = {
             TableName: "Scubamate",
             Key: {
-                'AccountGuid' : guid,
+                'AccountGuid' : AccountGuid,
             },
-            UpdateExpression: 'set AccountType = :a, InstructorNumber = :i, DiveCentre = :d',
+            UpdateExpression: 'set AccountType = :a, InstructorNumber = :i, DiveCentre = :d, AccountVerified = :av',
             ExpressionAttributeValues: {
                 ':a' : AccountType,
                 ':i' : InstructorNumber,
                 ':d': DiveCentre,
+                ':av': false,
+                
             }
-        }
+        };
         try{
             const data = await documentClient.update(params).promise();
             responseBody = "Successfully upgraded account!";
@@ -148,9 +122,9 @@ exports.handler = async (event, context, callback) => {
         },
         body : JSON.stringify(responseBody),
         isBase64Encoded: false
-    }
+    };
     return response;
     
-}
+};
 
 
