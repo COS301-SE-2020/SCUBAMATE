650e5dcde6bdf72a8083ba7ac006c792c39c7300
GiseleNadine <u18012583@tuks.co.za>
2020-08-04 17:34:51 +0200

    updated for courses

--- AWScode/getCourseRecommendation/index.js
+++ AWScode/getCourseRecommendation/index.js
@@ -6,9 +6,6 @@ exports.handler = async (event, context) => {
     
     const body = JSON.parse(event.body);
     const AccessToken = body.AccessToken; 
-    /* Qualification and Specialisation of user */
-    const Qualification = body.Qualification; 
-    const Specialisation = body.Specialisation; 
     
     const GuidSize = 36;
     const AccountGuid = AccessToken.substring(0,GuidSize);
@@ -73,83 +70,38 @@ exports.handler = async (event, context) => {
             responseBody = "Access Token Expired!";
             statusCode = 403;
         }
-        else if(!data.Item.AccountVerified){
-            statusCode = 403;
-            responseBody = "Account Not Verified by Admin - Ask your Dive Centre to Verify";
-        }
         else if(!data.Item.EmailVerified){
             statusCode = 403;
             responseBody = "Account Email Not Verified - Can't Upgrade Until Email Is Verified";
         }
         else{
-            /* Get List Of Qualification Courses which can be taken by this level */
+            /* Get List Of Courses */
             const paramsCourse = {
                 TableName: 'DiveInfo',
-                FilterExpression: 'begins_with(#itemT , :itemT) AND #QNeeded = :qual',
+                FilterExpression: 'begins_with(#itemT , :itemT)',
                 ExpressionAttributeNames: {
                     '#itemT': 'ItemType',
-                    '#QNeeded': 'QualificationNeeded',
                 },
                 ExpressionAttributeValues: {
                     ':itemT': "C-",
-                    ':qual': Qualification,
                 }
             };
 
             try{
                 const dataC = await documentClient.scan(paramsCourse).promise();
-                let tmpQ = [];
-                let tmpS = [];
+                let tmp = [];
                 dataC.Items.forEach(function(item) {
-                    tmpQ.push(item.Name);
+                    if(!contains(data.Item.CompletedCourses, item.Name)){
+                        tmp.push(item);
+                    }
                 });
-                if(tmpQ.length == 0){
-                    tmpQ = "No Qualification Courses Found";
+                if(tmp.length == 0){
+                    tmp= "No Courses Found";
                 }
-                /* Get List Of Specialisation Courses which can be taken by this level */
-                const paramsSCourse = {
-                    TableName: 'DiveInfo',
-                    FilterExpression: 'begins_with(#itemT , :itemT)',
-                    ExpressionAttributeNames: {
-                        '#itemT': 'ItemType',
-                    },
-                    ExpressionAttributeValues: {
-                        ':itemT': "S-",
-                    }
-                };
-    
-                try{
-                    const dataS = await documentClient.scan(paramsSCourse).promise();
-                    
-                    dataS.Items.forEach(function(item) {
-                        let itemName = item.Name;
-                        let itemQualAllowed = item.QualificationsAllowed;
-                        /*For each specialization
-                        * check if one of them is the 
-                        * same as account's then
-                        * check qualifications allowed
-                        * then add name of specialization*/
-                        if(!contains(Specialisation,itemName)){
-                            itemQualAllowed.forEach(function(qualAllowed) {
-                                if((qualAllowed==Qualification) && !contains(tmpS,itemName)){
-                                    tmpS.push(itemName);
-                                }
-                            });
-                        }
-                            
-                    });
-                    if(tmpS.length == 0){
-                        tmpS = "No Specialisation Courses Found";
-                    }
-                    let returnList = [];
-                    returnList.push({QualificationCourses: tmpQ});
-                    returnList.push({SpecialisationCourses: tmpS});
-                    responseBody = returnList;
-                    statusCode = 200;
-                }catch(err){
-                    responseBody = "Unable to find new courses"+err;
-                    statusCode = 403;
-                }
+                let returnList = [];
+                returnList.push({Courses: tmp});
+                responseBody = returnList[0];
+                statusCode = 200;
                   
             }catch(err){
                 responseBody = "Unable to find new courses"+err;
