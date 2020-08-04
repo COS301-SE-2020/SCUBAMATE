0d94c1814f2bb13613f88953f1f66cb193cf5965
GiseleNadine <u18012583@tuks.co.za>
2020-08-04 19:08:50 +0200

    Fixed bug with database

--- AWScode/scubamateChecklist/index.js
+++ AWScode/scubamateChecklist/index.js
@@ -14,11 +14,11 @@ exports.handler = async (event) => {
     if (dType === "Cave Dive" || dType === "Deep Dive" || dType === "Ice Dive" || dType === "Night Dive" || dType === "Reef Dive" || dType === "Wreck Dive")
     {
         //do nothing
-        dType = "DT-"+dType;
+        dType = "DT-"+dType.toLowerCase();
     }
     else
     {
-        dType = "DT-Other";
+        dType = "DT-other";
     }
     
     
@@ -44,90 +44,6 @@ exports.handler = async (event) => {
             responseBody = "Unable to retrieve checklist";
             statusCode = 404;
     }
-    
-    /*
-    var exposureSuit = "Wetsuit";
-    var night = false;
-    
-    if (dType=="Ice Dive" || dType=="Deep Dive")
-    {
-        exposureSuit = "Drysuit";
-    }
-    if (dType=="Reef Dive")
-    {
-        exposureSuit = "Dive skin";
-    }
-    if (dType == "Other")
-    {
-        exposureSuit = "Exposure suit";
-    }
-    
-    if (dType == "Night Dive" || dType == "Cave Dive")
-    {
-        night = true;
-    }
-    
-    
-    var responseBody;
-    if (night==true)
-    {
-        responseBody = {
-            Equipment : {
-                Mask : "Diving mask",
-                Suit : exposureSuit,
-                Fins : "Swimming fins",
-                Gloves : "Scuba gloves",
-                Tank : "Scuba tank",
-                Regulator : "Regulator",
-                DepthGuage: "Depth guage",
-                SPG : "Submersible pressure guage (SPG)",
-                Compass: "Compass",
-                DiveComputer : "Dive computer",
-                Light: "Underwater lights",
-                Bouyancy: "Buoyancy compensator"
-            },
-            Optional : {
-                Snorkel : "Snorkel",
-                Camera : "Underwater camera",
-                TB: "Tank bangers",
-                Defogger: "Defogger",
-                Knife: "Dive knives",
-                Slate: "Writing slates",
-
-                FA: "First aid kit"
-            }
-        };
-    }
-    else
-    {
-        responseBody = {
-            Equipment : {
-                Mask : "Diving mask",
-                Suit : exposureSuit,
-                Fins : "Swimming fins",
-                Gloves : "Scuba gloves",
-                Tank : "Scuba tank",
-                Regulator : "Regulator",
-                DepthGuage: "Depth guage",
-                SPG : "Submersible pressure guage (SPG)",
-                Compass: "Compass",
-                DiveComputer : "Dive computer",
-                Bouyancy: "Buoyancy compensator"
-            },
-            Optional : {
-                Snorkel : "Snorkel",
-                Camera : "Underwater camera",
-                TB: "Tank bangers",
-                Defogger: "Defogger",
-                Knife: "Dive knives",
-                Slate: "Writing slates",
-                Light: "Underwater lights",
-                FA: "First aid kit"
-            }
-        };
-    }
-    */
-    
     
     const response = {
         statusCode: statusCode,
