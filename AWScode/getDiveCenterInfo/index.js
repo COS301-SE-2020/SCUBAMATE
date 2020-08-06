1d5571bb2396afd115f67d8c033fd6d5e94fd95f
GiseleNadine <u18012583@tuks.co.za>
2020-08-06 08:36:07 +0200

    Update index.js

--- AWScode/getDiveCenterInfo/index.js
+++ AWScode/getDiveCenterInfo/index.js
@@ -11,58 +11,76 @@ exports.handler = async (event, context) => {
     const UserEntry = (body.UserEntry).toLowerCase(); 
     /* Page number to display - starting with Page 1*/
     const PageNum = body.PageNum; 
-    
-    let filter = 'begins_with(#itemT , :itemT) AND contains(#itemT , :user)';
-    let expressVal = {
-            ':itemT': ItemType,
-            ':user': UserEntry,
-        };
-        
-    if(UserEntry.toString().trim() === '*'){
-       filter = 'begins_with(#itemT , :itemT)';
-       expressVal = {
-            ':itemT': ItemType,
-        };
+    const validItemTypes = ["DS-","DC-"];
+    function contains(arr,search){
+        let returnBool = false;
+        arr.forEach(function(item) {
+            if(item==search){
+                returnBool=true;
+            }
+        });
+        return returnBool;
     }
-    
-    const params = {
-        TableName: 'DiveInfo',
-        FilterExpression: filter,
-        ExpressionAttributeNames: {
-            '#itemT': 'ItemType',
-        },
-        ExpressionAttributeValues: expressVal,
-    };
     
     let statusCode;
     let responseBody;
-    const documentClient = new AWS.DynamoDB.DocumentClient({region: "af-south-1"});
-    
-    try{
-        const data = await documentClient.scan(params).promise();
-        let tmp = [];
+    if(contains(validItemTypes, ItemType)){
+          
+        let filter = 'begins_with(#itemT , :itemT) AND contains(#itemT , :user)';
+        let expressVal = {
+                ':itemT': ItemType,
+                ':user': UserEntry,
+            };
+            
+        if(UserEntry.toString().trim() === '*'){
+           filter = 'begins_with(#itemT , :itemT)';
+           expressVal = {
+                ':itemT': ItemType,
+            };
+        }
         
-        const numOfItems = 10;
-        /*Show next ten items for current page */
-        for(let i=(PageNum-1);i<numOfItems;i++){
-            if(data.Items[i]!=null){
-                tmp.push(data.Items[i]);
+        const params = {
+            TableName: 'DiveInfo',
+            ProjectionExpression: "#name, Description, Coords, LogoPhoto, Courses, Instructors, DiveSites", 
+            FilterExpression: filter,
+            ExpressionAttributeNames: {
+                '#itemT': 'ItemType',
+                '#name' : 'Name'
+            },
+            ExpressionAttributeValues: expressVal,
+        };
+        const documentClient = new AWS.DynamoDB.DocumentClient({region: "af-south-1"});
+        
+        try{
+            const data = await documentClient.scan(params).promise();
+            let tmp = [];
+            
+            const numOfItems = 10;
+            /*Show next ten items for current page */
+            for(let i=(PageNum-1);i<numOfItems;i++){
+                if(data.Items[i]!=null){
+                    tmp.push(data.Items[i]);
+                }
             }
+            
+            if(tmp.length == 0){
+                responseBody = "No Results Found For: "+UserEntry;
+                statusCode = 404;
+            }
+            else{
+                let returnList = [];
+                returnList.push({ReturnedList: tmp});
+                responseBody = returnList[0];
+                statusCode = 200;
+            }
+            
+        }catch(err){
+            responseBody = "Unable to get data: "+err;
+            statusCode = 403;
         }
-        
-        if(tmp.length == 0){
-            responseBody = "No Results Found For: "+UserEntry;
-            statusCode = 404;
-        }
-        else{
-            let returnList = [];
-            returnList.push({ReturnedList: tmp});
-            responseBody = returnList[0];
-            statusCode = 200;
-        }
-        
-    }catch(err){
-        responseBody = "Unable to get data: "+err;
+    }
+    else{
+        responseBody = "Invalid Request.";
         statusCode = 403;
     }
 
