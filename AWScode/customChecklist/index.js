42473b831e118f430bdd8f7459da7d2358da6a64
JamesJenkinsFerrett <61706125+JamesJenkinsFerrett@users.noreply.github.com>
2020-08-07 09:27:40 +0200

    Improved customChecklist
    Improved the code for the customChecklist lambda funciton

--- AWScode/customChecklist/index.js
+++ AWScode/customChecklist/index.js
@@ -5,49 +5,83 @@ const documentClient = new AWS.DynamoDB.DocumentClient({region: "af-south-1"});
 
 exports.handler = async (event, context) => {
     // TODO implement
-    //let body = JSON.parse(event.body);
-    /*
-    const AccountGuid = body.AccountGuid;
+    let body = JSON.parse(event.body);
+    
+    const AccessToken = body.AccessToken;
     const Equipment = body.Equipment;
     const Optional = body.Optional;
     const Custom = body.Custom;
-    */
-    console.log("reddit moment");
     
-    const AccountGuid = event.AccountGuid;
+    
+    //const AccountGuid = event.AccountGuid;
+    
+    var AccountGuid;
+    
+    /*
+    const AccessToken = event.AccessToken;
     const Equipment = event.Equipment;
     const Optional = event.Optional;
     const Custom = event.Custom;
+    */
     
-    console.log("reddit moment");
     
-    let statusCode = 0;
-    let responseBody = "";
+    let statusCode = 200;
+    let responseBody = "Successfully added custom checklist";
     
-    const params = {
-        TableName: "Scubamate",
-        Key: {
-            AccountGuid: AccountGuid
+    
+    const scanParams = {
+        TableName : "Scubamate",
+        FilterExpression: "#ac = :ac",
+        ExpressionAttributeNames:{
+            '#ac' : 'AccessToken',
         },
-        UpdateExpression: "set Equipment = :e, Optional = :o, Custom = :c",
         ExpressionAttributeValues:{
-            ":e": Equipment,
-            ":o": Optional,
-            ":c": Custom
-        },
-        ReturnValues:"UPDATED_NEW"
+            ':ac': AccessToken,
+        }
     };
     
-    try{
-    const updateStatement = await documentClient.update(params).promise();
-    }catch(err){
-        console.log("damn");
-    }    
-    
+    const data = await documentClient.scan(scanParams).promise();
+    if (data.length==0)
+    {
+        responseBody = "Access Token not found ";
+        statusCode = 404;
+    }
+    else
+    {
+        AccountGuid = data.Items[0].AccountGuid;
+        
+        const params = {
+            TableName: "Scubamate",
+            Key: {
+                AccountGuid: AccountGuid
+            },
+            UpdateExpression: "set Equipment = :e, Optional = :o, Custom = :c",
+            ExpressionAttributeValues:{
+                ":e": Equipment,
+                ":o": Optional,
+                ":c": Custom
+            },
+            ReturnValues:"UPDATED_NEW"
+        };
+        
+        try{
+        const updateStatement = await documentClient.update(params).promise();
+        }catch(err){
+            console.log("damn");
+            statusCode = 404;
+            responseBody = "Update failed"
+        }    
+    }
     
     const response = {
-        statusCode: 200,
-        body: JSON.stringify('Hello from Lambda!'),
+        statusCode: statusCode,
+        headers: {
+            "Access-Control-Allow-Origin" : "*",
+            "Access-Control-Allow-Methods" : "OPTIONS,POST,GET",
+            "Access-Control-Allow-Credentials" : true,
+            "Content-Type" : "application/json"
+        },
+        body: JSON.stringify(responseBody),
     };
     return response;
 };
