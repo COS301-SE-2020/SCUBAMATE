f99768da3936a36287a5e3708e2f5fcb0e8cf1e3
CarmenJvR <50364617+CarmenJvR@users.noreply.github.com>
2020-08-13 11:45:09 +0200

    MultiStep New Log Form

--- src/app/service/account.service.ts
+++ src/app/service/account.service.ts
@@ -100,8 +100,23 @@ export class accountService
           "ItemType" : "A" ,
           "UserEntry" : entry
         } ;
-        console.log("Buddy Req");
-        console.log(body);
+
+
+       return this.httpClient.post('https://8shtmsbbn8.execute-api.af-south-1.amazonaws.com/getUser/lookbuddy',body, options);
+    }
+
+    lookAheadInstructor(entry : String): Observable<any>{
+
+      const options = {
+          headers: new HttpHeaders({
+            'Content-Type': 'application/json',
+          })
+        };
+
+      var  body= {
+          "ItemType" : "I" ,
+          "UserEntry" : entry
+        } ;
 
        return this.httpClient.post('https://8shtmsbbn8.execute-api.af-south-1.amazonaws.com/getUser/lookbuddy',body, options);
     }
