3d1dafa1112c14ad0e241a021ddd03c4914b4622
u17021627 <u17021627@tuks.co.za>
2020-08-17 18:04:05 +0200

    Unit/Integrated Test Home DONE

--- src/app/home/home.page.spec.ts
+++ src/app/home/home.page.spec.ts
@@ -9,7 +9,7 @@ import { Router } from '@angular/router';
 
 var accessToken = "d1d7391d-c035-28ab-0193-68a7d263d4be11ac76afb3c161…0702085a1c423b0ed53f38b9a0e6e0ad8bfe8cd3712f14be7";
 
-fdescribe('HomePage', () => {
+describe('HomePage', () => {
   let component: HomePage;
   let fixture: ComponentFixture<HomePage>;
   let accService: accountService;
@@ -30,33 +30,33 @@ fdescribe('HomePage', () => {
     router = TestBed.get(Router);
   }));
 
-  fit('Successfully Created Home Page', () => {
+  it('Successfully Created Home Page', () => {
     expect(component).toBeTruthy();
   });
 
-  fit('Testing Home Components', () => {
+  it('Testing Home Components', () => {
     expect(component.siteLst).toBeUndefined();
     expect(component.loginLabel).toBeDefined();
   });
 
-  fit('Testing ngOnInit()', () => {
+  it('Testing ngOnInit()', () => {
     localStorage.setItem("accessToken", accessToken);
     component.ngOnInit();
     expect(component.loginLabel).toBe("Log Out");
   });
 
-  fit('Testing ionViewWillEnter()', () => {
+  it('Testing ionViewWillEnter()', () => {
     localStorage.setItem("accessToken", accessToken);
     component.ionViewWillEnter();
     expect(component.loginLabel).toBe("Log Out");
   });
 
-  fit('Testing loginClick()', () => {
+  it('Testing loginClick()', () => {
     component.loginClick();
     expect(localStorage.getItem("accessToken")).toBeDefined();
   });
 
-  fit('Testing Home Functionality', () => {
+  it('Testing Home Functionality', () => {
     expect(component.ngOnInit).toBeTruthy();
     expect(component.ionViewWillEnter).toBeTruthy();
     expect(component.loginClick).toBeTruthy();
