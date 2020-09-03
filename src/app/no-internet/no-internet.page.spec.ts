ca7645484f8819ffcf34cb288f0c46348c460fed
u17021627 <u17021627@tuks.co.za>
2020-09-03 16:39:29 +0200

    Unit/Integrated Test NoInternet V.01

--- src/app/no-internet/no-internet.page.spec.ts
+++ src/app/no-internet/no-internet.page.spec.ts
@@ -1,21 +1,32 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
-
 import { NoInternetPage } from './no-internet.page';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpTestingController, HttpClientTestingModule } from '@angular/common/http/testing';
+import {HttpModule} from '@angular/http';
+import { HttpClient} from '@angular/common/http';
+import { Router } from '@angular/router';
 
 describe('NoInternetPage', () => {
   let component: NoInternetPage;
   let fixture: ComponentFixture<NoInternetPage>;
+  let http: HttpClient;
+  let router; Router;
+  let httpMock: HttpTestingController;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ NoInternetPage ],
-      imports: [IonicModule.forRoot()]
+      imports: [IonicModule.forRoot(), RouterTestingModule.withRoutes([]), HttpClientTestingModule, HttpModule],
+      providers: [HttpModule]
     }).compileComponents();
 
     fixture = TestBed.createComponent(NoInternetPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    router = TestBed.get(Router);
+    httpMock = TestBed.get(HttpTestingController);
+    http = TestBed.get(HttpClient);
   }));
 
   it('should create', () => {
