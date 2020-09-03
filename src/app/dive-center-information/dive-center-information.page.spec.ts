00590c2f2dc076c2bec065da88f30b1cb4f1a8d5
u17021627 <u17021627@tuks.co.za>
2020-09-03 14:15:04 +0200

    Unit/Integrated Test Dive Center Info V.02

--- src/app/dive-center-information/dive-center-information.page.spec.ts
+++ src/app/dive-center-information/dive-center-information.page.spec.ts
@@ -1,25 +1,22 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 import { DiveCenterInformationPage } from './dive-center-information.page';
-import { accountService } from '../service/account.service';
 import { diveService } from '../service/dive.service';
 import { weatherService } from '../service/weather.service';
 import { HttpClient} from '@angular/common/http';
 import { Router } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
-import { AppModule } from '../app.module';
 import { HttpTestingController, HttpClientTestingModule } from '@angular/common/http/testing';
 import {HttpModule} from '@angular/http';
-import { Key } from 'protractor';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
 
 var validData = {
   accessToken : "d1d7391d-c035-28ab-0193-68a7d263d4be11ac76afb3c161…0702085a1c423b0ed53f38b9a0e6e0ad8bfe8cd3712f14be7"
 };
 
-fdescribe('DiveCenterInformationPage', () => {
+describe('DiveCenterInformationPage', () => {
   let component: DiveCenterInformationPage;
   let fixture: ComponentFixture<DiveCenterInformationPage>;
-  let accService: accountService;
   let divService: diveService;
   let weatService: weatherService;
   let http: HttpClient;
@@ -29,16 +26,14 @@ fdescribe('DiveCenterInformationPage', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ DiveCenterInformationPage ],
-      imports: [IonicModule.forRoot(), RouterTestingModule.withRoutes([]), AppModule, HttpClientTestingModule, HttpModule],
-      providers: [accountService, diveService, weatherService, HttpModule]
+      imports: [IonicModule.forRoot(), RouterTestingModule.withRoutes([]), HttpClientTestingModule, HttpModule],
+      providers: [diveService, weatherService, HttpModule, Geolocation]
     }).compileComponents();
 
     fixture = TestBed.createComponent(DiveCenterInformationPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
-    localStorage.setItem("accessToken", validData.accessToken);
-    accService = new accountService(http, router);
-    divService = new diveService(http, router);
+    divService = TestBed.get(diveService);
     weatService = TestBed.get(weatherService);
     router = TestBed.get(Router);
     httpMock = TestBed.get(HttpTestingController);
@@ -48,20 +43,46 @@ fdescribe('DiveCenterInformationPage', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  
+  it('getSingleDiveCenter() test', () => {
+    var center = "Clifton Rock";
 
-  fit('getLocationKey() test', () => {
-    var coords = {
-      Latitude: "-25.840380",
-      Longtitude: "28.245230"
-    }
-
-    var tempKey;
-
-    weatService.getLocationKey(coords).subscribe((resp : any) => {
-      tempKey = resp.Key;
-      console.log(tempKey);
+    divService.getSingleDiveCenter(center).subscribe((resp : any) => {
+      console.log(resp);
     });
 
-    expect(coords.Latitude).toBe("-25.840380");
+    expect(center).toBe("Clifton Rock");
   });
+
+  it('getLocationKey() test', () => {
+    var Coordinates = {
+      Latitude: "-25.840380",
+      Longitude: "28.245230"
+    };
+
+    weatService.getLocationKey(Coordinates).subscribe((resp : any) => {
+      console.log(resp);
+    });
+
+    expect(Coordinates.Latitude).toBe("-25.840380");
+    expect(Coordinates.Longitude).toBe("28.245230");
+  });
+
+  it('getLogWeather() test', () => {
+    
+    var Key = {
+      key: null,
+      city: null,
+      province: null
+    };
+
+    weatService.getLogWeather(Key).subscribe((resp : any) => {
+      console.log(resp);
+    });
+
+    expect(Key.key).toBeNull();
+    expect(Key.city).toBeNull();
+    expect(Key.province).toBeNull();
+  });
+  
 });
