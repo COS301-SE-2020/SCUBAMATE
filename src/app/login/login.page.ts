6dfc2fe037b82900d9002e69e9397a5328c1d315
CarmenJvR <50364617+CarmenJvR@users.noreply.github.com>
2020-09-10 09:26:08 +0200

    Deployment Bug Fix

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -129,7 +129,7 @@ export class LoginPage implements OnInit {
     }
   }
 
-  onSubmit(iEmail: string, iPass: string , event : Event) {
+  onSubmit(iEmail: any , iPass: any , event : Event) {
      var attemptLogin = {Email: iEmail, Password: iPass} as LoginClass; 
      
 
