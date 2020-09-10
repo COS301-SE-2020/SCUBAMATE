0e62f151646e651b2d08013f19249d6fa24b8a94
u17021627 <u17021627@tuks.co.za>
2020-09-10 14:47:58 +0200

    mixed mock and spies

--- src/app/explore/explore.page.spec.ts
+++ src/app/explore/explore.page.spec.ts
@@ -10,6 +10,8 @@ import { HttpClient} from '@angular/common/http';
 import { Router } from '@angular/router';
 import { FormBuilder} from '@angular/forms';
 
+var accessToken = "d1d7391d-c035-28ab-0193-68a7d263d4be11ac76afb3c161…0702085a1c423b0ed53f38b9a0e6e0ad8bfe8cd3712f14be7";
+
 describe('ExplorePage', () => {
   let component: ExplorePage;
   let fixture: ComponentFixture<ExplorePage>;
@@ -34,6 +36,7 @@ describe('ExplorePage', () => {
     router = TestBed.get(Router);
     httpMock = TestBed.get(HttpTestingController);
     http = TestBed.get(HttpClient);
+    localStorage.setItem("accessToken", accessToken);
   }));
 
   it('should create', () => {
@@ -58,6 +61,77 @@ describe('ExplorePage', () => {
     });
 
     expect(eventValue).toBe("");
+  });
+
+  it('Testing Explore Components', () => {
+    expect(component.siteLst).toBeDefined();
+    expect(component.centerLst).toBeDefined();
+    expect(component.showSites).toBeFalse();
+    expect(component.showCenters).toBeFalse();
+    expect(component.showFeed).toBeTrue();
+    expect(component.showLoading).toBeTrue();
+    expect(component.pubLst).toBeUndefined();
+    expect(component.loginLabel).toBe("Log Out");
+  });
+
+  it('Testing Explore Functionlity', () => {
+    expect(component.ngOnInit).toBeDefined();
+    expect(component.ionViewWillEnter).toBeDefined();
+    expect(component.loginClick).toBeDefined();
+    expect(component.displayDiveSites).toBeDefined();
+    expect(component.displayDiveCenters).toBeDefined();
+    expect(component.displayFeed).toBeDefined();
+  });
+
+  it('Testing ngOnInit()', () => {
+    component.ngOnInit();
+    expect(component.showFeed).toBeTrue();
+    expect(component.showSites).toBeFalse();
+    expect(component.showCenters).toBeFalse();
+    expect(component.loginLabel).toBe("Log Out");
+    expect(component.pubLst).toBeUndefined();
+    expect(component.showLoading).toBeTrue();
+    let diveSpy = spyOn(divService, 'getPublicDives').and.callThrough();
+    expect(diveSpy).toBeDefined();
+  });
+
+  it('Testing ionViewWillEnter', () => {
+    component.ngOnInit();
+    expect(component.showFeed).toBeTrue();
+    expect(component.showSites).toBeFalse();
+    expect(component.showCenters).toBeFalse();
+    expect(component.loginLabel).toBe("Log Out");
+    expect(component.pubLst).toBeUndefined();
+    expect(component.showLoading).toBeTrue();
+    let diveSpy = spyOn(divService, 'getPublicDives').and.callThrough();
+    expect(diveSpy).toBeDefined();
+  });
+
+  it('Testing displayDiveSites', () => {
+    component.displayDiveSites();
+    expect(component.showFeed).toBeFalse();
+    expect(component.showSites).toBeTrue();
+    expect(component.showCenters).toBeFalse();
+    let diveSpy = spyOn(divService, 'getDiveSites').and.callThrough();
+    expect(diveSpy).toBeDefined();
+    expect(component.showLoading).toBeTrue();
+  });
+
+  it('Testing displayDiveCenters', () => {
+    component.displayDiveSites();
+    expect(component.showFeed).toBeFalse();
+    expect(component.showSites).toBeTrue();
+    expect(component.showCenters).toBeFalse();
+    let diveSpy = spyOn(divService, 'getDiveCenters').and.callThrough();
+    expect(diveSpy).toBeDefined();
+    expect(component.showLoading).toBeTrue();
+  });
+
+  it('Testing displayFeed', () => {
+    component.displayFeed();
+    expect(component.showFeed).toBeTrue();
+    expect(component.showSites).toBeFalse();
+    expect(component.showCenters).toBeFalse();
   });
 
 });
